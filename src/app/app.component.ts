5f0a2c4205063d698321911209d183f7005194c2
Ke Jin <ke.jin@laserfiche.com>
2022-08-15 16:22:43 -0700

    use js-utils 4.0.3, change font style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { LfFieldContainerComponent, LfFolderBrowserComponent, LfLoginComponent, 
 import { getEntryWebAccessUrl } from './lf-url-utils';
 
 const resources: Map<string, object> = new Map<string, object>([
-  ['en', {
+  ['en-US', {
     'FOLDER_BROWSER_PLACEHOLDER': 'No folder selected',
     'SAVE_TO_LASERFICHE': 'Save to Laserfiche',
     'CLICK_TO_UPLOAD': 'Click to upload file',
