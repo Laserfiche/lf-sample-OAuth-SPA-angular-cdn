2c165e0f5d4a23503e8dbc635ea1949811a429d1
Terrence James Diaz <terrence.diaz@laserfiche.com>
2023-02-08 16:37:25 -0800

    Clean up docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,8 +39,8 @@ interface ILfSelectedFolder {
 export class AppComponent implements AfterViewInit {
   REDIRECT_URI: string = 'REPLACE_WITH_YOUR_REDIRECT_URI'; // i.e http://localhost:3000, https://serverName/lf-sample/index.html
   CLIENT_ID: string = 'REPLACE_WITH_YOUR_CLIENT_ID';
-  HOST_NAME: string = 'laserfiche.com'; // only update this if you are using a different region or environment (i.e. laserfiche.ca, eu.laserfiche.com)
-  SCOPE: string = "repository.Read"; // add what scopes you would like in the authorization request, i.e. repository.Read, repository.ReadWrite
+  HOST_NAME: string = 'laserfiche.com'; // only update this if you are using a different environment (i.e. a.clouddev.laserfiche.com)
+  SCOPE: string = 'repository.Read repository.Write'; // Scope(s) requested by the app
 
   // repository client that will be used to connect to the LF API
   private repoClient?: IRepositoryApiClientExInternal;
@@ -129,7 +129,7 @@ export class AppComponent implements AfterViewInit {
     const accessToken = this.loginComponent.nativeElement.authorization_credentials.accessToken;
     if (accessToken) {
       request.headers['Authorization'] = 'Bearer ' + accessToken;
-      return { regionalDomain: this.HOST_NAME }; // update this if you want CA, EU, dev
+      return { regionalDomain: this.loginComponent.nativeElement.account_endpoints.regionalDomain };
     }
     else {
       throw new Error('Access Token undefined.');
